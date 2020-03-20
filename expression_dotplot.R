e82866403a15e32c5d517ab418d3cb6bf18cd820
leonardblaschek <l.blaschek@vivaldi.net>
2020-03-20 15:52:56 +0100

    update review figures

--- expression_dotplot.R
+++ expression_dotplot.R
@@ -1,51 +1,107 @@
-library(ggplot2)
+library(tidyverse)
 library(ggthemes)
 library(showtext)
-library(dplyr)
-library(tidyr)
+library(ggrepel)
 
+
+#### import Helvetica Neue ####
 font_add(
   "Helvetica",
-  regular = "/prop_fonts/01. Helvetica     [1957 - Max Miedinger]/HelveticaNeueLTStd-Lt.otf",
-  italic = "/prop_fonts/01. Helvetica     [1957 - Max Miedinger]/HelveticaNeueLTStd-LtIt.otf",
-  bold = "/prop_fonts/01. Helvetica     [1957 - Max Miedinger]/HelveticaNeueLTStd-Bd.otf",
-  bolditalic = "/prop_fonts/01. Helvetica     [1957 - Max Miedinger]/HelveticaNeueLTStd-BdIt.otf"
+  regular = "/prop_fonts/01. Helvetica   [1957 - Max Miedinger]/HelveticaNeueLTStd-Lt.otf",
+  italic = "/prop_fonts/01. Helvetica   [1957 - Max Miedinger]/HelveticaNeueLTStd-LtIt.otf",
+  bold = "/prop_fonts/01. Helvetica   [1957 - Max Miedinger]/HelveticaNeueLTStd-Bd.otf",
+  bolditalic = "/prop_fonts/01. Helvetica   [1957 - Max Miedinger]/HelveticaNeueLTStd-BdIt.otf"
 )
 showtext_auto()
 
 #### generating plot theme ####
-theme_leo <- function(base_size = 12,
-                      base_family = "Helvetica"){
-  theme_minimal(base_size = base_size,
-                base_family = base_family) %+replace%
+theme_leo <- function(base_size = 6,
+                      base_family = "Helvetica") {
+  theme_minimal(
+    base_size = base_size,
+    base_family = base_family
+  ) %+replace%
     theme(
-      strip.text = element_text(
-        hjust = 0, 
-        face = "italic"
-      ),
+      strip.text = element_text(hjust = 0, face = "italic"),
       axis.ticks = element_line(
-        size = 0.25,
+        size = 0.125,
         lineend = "square",
         color = "black"
       ),
-      axis.title = element_blank(),
-      axis.text.y = element_text(colour = "black", 
-                                 margin = margin(t = 0, r = 2.5, b = 0, l = 0)),
       axis.text.x = element_text(
+        size = 6,
+        colour = "black", # flipped coords
+        margin = margin(1, 1, 1, 1)
+      ),
+      axis.text.y = element_text(
         colour = "black",
+        size = 6,
         angle = 0,
         vjust = 0.5,
-        hjust = 0.5
+        hjust = 1,
+        margin = margin(1, 1, 1, 1)
+      ),
+      axis.title = element_text(
+        colour = "black",
+        size = 6
       ),
       panel.grid.major = element_blank(),
       panel.grid.minor = element_blank(),
       panel.border = element_rect(fill = NA, color = "black", size = 0.25),
       panel.spacing = unit(1.5, "mm"),
       legend.position = "none",
-      
+      legend.text = element_text(size = 6),
+      legend.key.height = unit(4, "mm"),
+      plot.title = element_text(
+        size = 6,
+        hjust = 0
+      ),
       complete = TRUE
     )
 }
+
+equal_breaks <- function(n = 3, s = 0.05, ...) {
+  function(value) {
+    # rescaling
+    d <- s * diff(range(value)) / (1 + 2 * s)
+    round(seq(0, max(value) - d, length = n), digits = 2)
+  }
+}
+
+my_breaks <- function(value) {
+  if (
+    max(value) < 0.005) {
+    seq(0, 0.004, length.out = 3)
+  } else if (
+    max(value) < 0.05) {
+    seq(0, 0.02, length.out = 3)
+  } else if (
+    max(value) < 2) {
+    seq(0, 1, length.out = 3)
+  } else {
+    seq(0, 100, length.out = 3)
+  }
+}
+
+my_limits <- function(value) {
+  if (
+    max(value) < 0.005) {
+    c(0, 0.0045)
+  } else if (
+    max(value) < 0.05) {
+    c(0, 0.025)
+  } else if (
+    max(value) < 2) {
+    c(-0.25, 1.1)
+  } else {
+    c(-10, 120)
+  }
+}
+
+ggtext_size <- 6 / (14 / 5)
+twocol <- 18 / 2.54
+onehalfcol <- 14 / 2.54
+onecol <- 9 / 2.54
 
 expression <- read.csv("file:///home/leonard/Dropbox/Review/figures_PO-review/expression_fig/expression.csv")
 expression$PO <- ordered(expression$PO, levels = c("PPO", "LAC", "PRX"))
@@ -66,61 +122,57 @@ ex_plot <- ggplot(expression.count, aes(x = compartment, y = number, fill = PO))
   geom_jitter(data = filter(expression.count, method == "pred"),
               aes(x = compartment, y = number, fill = PO),
               alpha = 0) +
-  geom_vline(xintercept = 1,
-             size = 7.5,
-             color = "grey95") +
-  geom_vline(xintercept = 3,
-             size = 7.5,
-             color = "grey95") +
-  geom_vline(xintercept = 5,
-             size = 7.5,
-             color = "grey95") +
-  geom_vline(xintercept = 7,
-             size = 7.5,
-             color = "grey95") +
+  annotate("rect",
+           ymin = -Inf,
+           ymax = Inf,
+           xmin = c(0.5, 2.5, 4.5, 6.5),
+           xmax = c(1.5, 3.5, 5.5, 7.5),
+           fill = "grey95",
+           colour = NA) +
+  # geom_vline(xintercept = 1,
+  #            size = 7.5,
+  #            color = "grey95") +
+  # geom_vline(xintercept = 3,
+  #            size = 7.5,
+  #            color = "grey95") +
+  # geom_vline(xintercept = 5,
+  #            size = 7.5,
+  #            color = "grey95") +
+  # geom_vline(xintercept = 7,
+  #            size = 7.5,
+  #            color = "grey95") +
   geom_jitter(data = filter(expression.count, method == "pred"), aes(x = compartment, y = number, fill = PO),
               width = 0.2,
               shape = 21,
               alpha = 0.5,
-              size = 1,
+              size = 0.5,
               stroke = 0.1) +
   geom_jitter(data = filter(expression.count, method == "exp"), aes(x = compartment, y = number, fill = PO),
               width = 0.2,
               shape = 21,
-              alpha = 0.9,
-              size = 2,
+              alpha = 0.75,
+              size = 1,
               stroke = 0.2) +
+  # geom_text_repel(data = filter(expression.count, 
+  #                               PO == "LAC" & 
+  #                                 compartment %in% c("Vacuole", "Thylakoid")),
+  #                 label = c("Experimental",
+  #                           "Prediction"),
+  #                 family = "Helvetica",
+  #                 size = ggtext_size,
+  #                 # direction = "x",
+  #                 hjust = 0,
+  #                 vjust = 0.5,
+  #                 segment.size = 0.2,
+  #                 min.segment.length = 0,
+  #                 nudge_y = 20) +
+  labs(y = "Reported protein localisations") +
   theme_leo() +
-  theme(axis.ticks.x = element_blank(),
-        axis.text.y = element_text(hjust = 1)) +
+  theme(axis.title.y = element_blank()) +
   scale_fill_few() +
   facet_wrap(~ PO, nrow = 1) +
   coord_flip()
 
-ex_plot_2 <- ggplot() +
-  geom_jitter(data = filter(expression.count, method == "pred"), aes(x = PO, y = number, fill = PO),
-              width = 0.2,
-              shape = 21,
-              alpha = 0.5,
-              size = 1,
-              stroke = 0.1) +
-  geom_jitter(data = filter(expression.count, method == "exp"), aes(x = PO, y = number, fill = PO),
-              width = 0.2,
-              shape = 21,
-              alpha = 0.9,
-              size = 2,
-              stroke = 0.2) +
-  theme_leo() +
-  theme(axis.text = element_blank(),
-        axis.ticks = element_blank()) +
-  scale_fill_few() +
-  facet_wrap(~ compartment, ncol = 1, strip.position = "right") +
-  coord_flip()
-
-pdf("expression_plot.pdf", height = 2, width = 6)
+pdf("expression_plot.pdf", height = onecol / 3, width = onecol)
 ex_plot
-dev.off()
-
-pdf("expression_plot_2.pdf", height = 7, width = 3)
-ex_plot_2
 dev.off()
--- lac_data.R
+++ lac_data.R
@@ -1,130 +1,261 @@
-library(tidyr)
-library(dplyr)
-library(ggplot2)
+library(tidyverse)
 library(ggthemes)
 library(showtext)
-library(reshape2)
 library(cowplot)
 library(ggbeeswarm)
+library(patchwork)
 
 
-# import Helvetica Neue
-font_add("Helvetica", regular = "/prop_fonts/01. Helvetica     [1957 - Max Miedinger]/HelveticaNeueLTStd-Lt.otf",
-         italic = "/prop_fonts/01. Helvetica     [1957 - Max Miedinger]/HelveticaNeueLTStd-LtIt.otf",
-         bold = "/prop_fonts/01. Helvetica     [1957 - Max Miedinger]/HelveticaNeueLTStd-Bd.otf")
+#### import Helvetica Neue ####
+font_add(
+  "Helvetica",
+  regular = "/prop_fonts/01. Helvetica   [1957 - Max Miedinger]/HelveticaNeueLTStd-Lt.otf",
+  italic = "/prop_fonts/01. Helvetica   [1957 - Max Miedinger]/HelveticaNeueLTStd-LtIt.otf",
+  bold = "/prop_fonts/01. Helvetica   [1957 - Max Miedinger]/HelveticaNeueLTStd-Bd.otf",
+  bolditalic = "/prop_fonts/01. Helvetica   [1957 - Max Miedinger]/HelveticaNeueLTStd-BdIt.otf"
+)
 showtext_auto()
 
-lac.data <- read.csv("file:///home/leonard/Dropbox/Review/lac_data.csv")
+#### generating plot theme ####
+theme_leo <- function(base_size = 6,
+                      base_family = "Helvetica") {
+  theme_minimal(
+    base_size = base_size,
+    base_family = base_family
+  ) %+replace%
+    theme(
+      strip.text = element_text(hjust = 0, face = "italic"),
+      axis.ticks = element_line(
+        size = 0.125,
+        lineend = "square",
+        color = "black"
+      ),
+      axis.text.x = element_text(
+        size = 6,
+        colour = "black", # flipped coords
+        margin = margin(1, 1, 1, 1)
+      ),
+      axis.text.y = element_text(
+        colour = "black",
+        size = 6,
+        angle = 0,
+        vjust = 0.5,
+        hjust = 1,
+        margin = margin(1, 1, 1, 1)
+      ),
+      axis.title = element_text(
+        colour = "black",
+        size = 6
+      ),
+      panel.grid.major = element_blank(),
+      panel.grid.minor = element_blank(),
+      panel.border = element_rect(fill = NA, color = "black", size = 0.25),
+      panel.spacing = unit(1.5, "mm"),
+      legend.position = "none",
+      legend.text = element_text(size = 6),
+      legend.key.height = unit(4, "mm"),
+      plot.title = element_text(
+        size = 6,
+        hjust = 0
+      ),
+      complete = TRUE
+    )
+}
 
-lac.short <- subset(lac.data, select = c(1,2,8,10:24))
-lac.short <- melt(lac.short, c("Kingdom", "Species", "Temperature.optimum", "PI"))
-lac.short <-
-  lac.short %>% separate(variable, c("var", "substrate"), extra = "merge", remove = FALSE)
-lac.short$value <- as.numeric(as.character(lac.short$value))
-lac.short$var <- as.factor(as.character(lac.short$var))
+equal_breaks <- function(n = 3, s = 0.05, ...) {
+  function(value) {
+    # rescaling
+    d <- s * diff(range(value)) / (1 + 2 * s)
+    round(seq(0, max(value) - d, length = n), digits = 2)
+  }
+}
 
-lac.short <- subset(lac.short, substrate != "GOH" & substrate != "LDOPA" & substrate != "PYGL" & substrate != "GUA")
-lac.short$Kingdom <- ordered(lac.short$Kingdom, levels = c("Fungi", "Prokaryota", "Plantae"))
-lac.short$Temperature.optimum <- as.numeric(as.character(lac.short$Temperature.optimum))
+my_breaks <- function(value) {
+  if (
+    max(value) < 0.005) {
+    seq(0, 0.004, length.out = 3)
+  } else if (
+    max(value) < 0.05) {
+    seq(0, 0.02, length.out = 3)
+  } else if (
+    max(value) < 2) {
+    seq(0, 1, length.out = 3)
+  } else {
+    seq(0, 100, length.out = 3)
+  }
+}
 
+my_limits <- function(value) {
+  if (
+    max(value) < 0.005) {
+    c(0, 0.0045)
+  } else if (
+    max(value) < 0.05) {
+    c(0, 0.025)
+  } else if (
+    max(value) < 2) {
+    c(-0.25, 1.1)
+  } else {
+    c(-10, 120)
+  }
+}
 
-lac.fig.km <- ggplot(data = subset(lac.short, var == "Km"), aes(x = Kingdom, y = value, fill = Kingdom)) +
-  geom_quasirandom(shape = 21, stroke = 0.1, size = 2, alpha = 0.75) +
-  geom_violin(draw_quantiles = 0.5, alpha = 0.25) +
-  # geom_jitter(shape = 21, width = 0.05, stroke = 0.1, size = 2, alpha = 0.75) +
-  # geom_boxplot(fill = NA, outlier.alpha = 0, width = 0.25, colour = "black") +
+ggtext_size <- 6 / (14 / 5)
+twocol <- 18 / 2.54
+onehalfcol <- 14 / 2.54
+onecol <- 9 / 2.54
+
+#### import data ####
+
+lac_data <- read_csv("/home/leonard/Dropbox/Review/2020_rewrite/Data/lac_data.csv") %>%
+  select(1, 2, 8, 10:24) %>%
+  pivot_longer(-c("Kingdom", "Species", "Temperature.optimum", "PI"),
+    names_to = "variable", values_to = "value"
+  ) %>%
+  separate(variable, c("var", "substrate"), extra = "merge", remove = TRUE) %>%
+  mutate(
+    value = as.numeric(value),
+    Temperature.optimum = as.numeric(Temperature.optimum),
+    Kingdom = ordered(Kingdom, levels = c("Fungi", "Prokaryota", "Plantae"))
+  ) %>%
+  filter(substrate %in% c("ABTS", "DMP", "SGZ"))
+
+#### Km figure ####
+
+lac.fig.km <- ggplot(
+  data = filter(lac_data, var == "Km"),
+  aes(x = Kingdom, y = value, fill = Kingdom)
+) +
+  geom_quasirandom(
+    aes(colour = Kingdom),
+    dodge.width = 0.6,
+    shape = 16,
+    alpha = 0.75,
+    size = 1
+  ) +
+  geom_violin(
+    draw_quantiles = 0.5,
+    fill = "white",
+    colour = "black",
+    alpha = 0.5,
+    width = 0.4,
+    position = position_dodge(width = 0.6),
+    size = 0.2,
+    scale = "width"
+  ) +
   scale_y_log10(limits = c(0.5, 10000)) +
   labs(y = "Km [µM]") +
-  scale_fill_brewer(palette = "Set1") +
-  theme_base() +
-  theme(text = element_text( family = "Helvetica"),
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
+  scale_colour_brewer(palette = "Set1") +
+  theme_leo() +
+  theme(axis.text.x = element_blank(),
         axis.title.x = element_blank(),
-        plot.background = element_blank(),
-        strip.text = element_blank(),
-        plot.margin = unit(c(0,1,-1,1), "mm"),
-        legend.position = "none") +
-  facet_wrap(~ substrate)
+        axis.ticks.x = element_blank()) +
+  facet_wrap(~substrate)
 
-lac.fig.ph <- ggplot(data = subset(lac.short, var == "pH"), aes(x = Kingdom, y = value, fill = Kingdom)) +
-  geom_quasirandom(shape = 21, stroke = 0.1, size = 2, alpha = 0.75) +
-  geom_violin(draw_quantiles = 0.5, alpha = 0.25) +
-  # geom_jitter(shape = 21, width = 0.05, stroke = 0.1, size = 2, alpha = 0.75) +
-  # geom_boxplot(fill = NA, outlier.alpha = 0, width = 0.25, colour = "black") +
+#### pH figure ####
+
+lac.fig.ph <- ggplot(data = filter(lac_data, var == "pH"), aes(x = Kingdom, y = value, fill = Kingdom)) +
+  geom_quasirandom(
+    aes(colour = Kingdom),
+    dodge.width = 0.6,
+    shape = 16,
+    alpha = 0.75,
+    size = 1
+  ) +
+  geom_violin(
+    draw_quantiles = 0.5,
+    fill = "white",
+    colour = "black",
+    alpha = 0.5,
+    width = 0.4,
+    position = position_dodge(width = 0.6),
+    size = 0.2,
+    scale = "width"
+  ) +
   labs(y = "pH optimum") +
-  scale_fill_brewer(palette = "Set1") +
+  scale_colour_brewer(palette = "Set1") +
   scale_y_continuous(limits = c(0, 10), breaks = c(0, 3, 6, 9)) +
-  theme_base() +
-  theme(text = element_text( family = "Helvetica"),
-        # axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
+  theme_leo() +
+  theme(strip.text = element_blank(),
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
         axis.title.x = element_blank(),
-        plot.background = element_blank(),
-        legend.position = "bottom",
-        legend.title = element_blank(),
-        plot.margin = unit(c(0,1,0,1), "mm")
-        ) +
-  facet_wrap(~ substrate, strip.position="bottom")
+        axis.ticks.x = element_blank()) +
+  facet_wrap(~substrate)
 
-lac.fig.temp <- ggplot(data = lac.short, aes(x = Kingdom, y = Temperature.optimum, fill = Kingdom)) +
-  geom_quasirandom(shape = 21, stroke = 0.1, size = 2, alpha = 0.75) +
-  geom_violin(draw_quantiles = 0.5, alpha = 0.25) +
-  # geom_jitter(shape = 21, width = 0.05, stroke = 0.1, size = 2, alpha = 0.5) +
-  # geom_boxplot(fill = NA, outlier.alpha = 0, width = 0.25, colour = "black") +
+#### Temp figure ####
+
+lac.fig.temp <- ggplot(data = lac_data, aes(x = Kingdom, y = Temperature.optimum, fill = Kingdom)) +
+  geom_quasirandom(
+    aes(colour = Kingdom),
+    dodge.width = 0.6,
+    shape = 16,
+    alpha = 0.75,
+    size = 1
+  ) +
+  geom_violin(
+    draw_quantiles = 0.5,
+    fill = "white",
+    colour = "black",
+    alpha = 0.5,
+    width = 0.4,
+    position = position_dodge(width = 0.6),
+    size = 0.2,
+    scale = "width"
+  ) +
+  annotate("text",
+           x = c(1, 2, 3),
+           y = c(5, 15, 25),
+           label = c("Fungi", "Prokaryota", "Plantae"),
+           family = "Helvetica",
+           size = ggtext_size
+  ) +
+  annotate("segment",
+           x = c(1, 2, 3),
+           xend = c(1, 2, 3),
+           y = c(22, 27, 37),
+           yend = c(8, 18, 28),
+           colour = "black",
+           size = 0.2
+  ) +
   labs(y = "Temperature optimum [°C]") +
-  scale_fill_brewer(palette = "Set1") +
+  scale_colour_brewer(palette = "Set1") +
   scale_y_continuous(limits = c(0, 95)) +
-  theme_base() +
-  theme(text = element_text( family = "Helvetica"),
-        # axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
+  theme_leo() +
+  theme(axis.text.x = element_blank(),
         axis.title.x = element_blank(),
-        plot.background = element_blank(),
-        legend.position = "none",
-        legend.title = element_blank()
-        # plot.margin = unit(c(0,0,0,0), "cm")
-  ) 
+        axis.ticks.x = element_blank())
 
-lac.fig.pi <- ggplot(data = lac.short, aes(x = Kingdom, y = PI, fill = Kingdom)) +
-  geom_quasirandom(shape = 21, stroke = 0.1, size = 2, alpha = 0.75) +
-  geom_violin(draw_quantiles = 0.5, alpha = 0.25) +
-  # geom_jitter(shape = 21, width = 0.05, stroke = 0.1, size = 2, alpha = 0.5) +
-  # geom_boxplot(fill = NA, outlier.alpha = 0, width = 0.25, colour = "black") +
+#### pI figure ####
+
+lac.fig.pi <- ggplot(data = lac_data, aes(x = Kingdom, y = PI, fill = Kingdom)) +
+  geom_quasirandom(
+    aes(colour = Kingdom),
+    dodge.width = 0.6,
+    shape = 16,
+    alpha = 0.75,
+    size = 1
+  ) +
+  geom_violin(
+    draw_quantiles = 0.5,
+    fill = "white",
+    colour = "black",
+    alpha = 0.5,
+    width = 0.4,
+    position = position_dodge(width = 0.6),
+    size = 0.2,
+    scale = "width"
+  ) +
   labs(y = "Isoelectric point") +
-  scale_fill_brewer(palette = "Set1") +
+  scale_colour_brewer(palette = "Set1") +
   scale_y_continuous(limits = c(0, 10.5), breaks = c(0, 3, 6, 9)) +
-  theme_base() +
-  theme(text = element_text( family = "Helvetica"),
-        # axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1),
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
+  theme_leo() +
+  theme(axis.text.x = element_blank(),
         axis.title.x = element_blank(),
-        plot.background = element_blank(),
-        legend.position = "none",
-        legend.title = element_blank()
-        # plot.margin = unit(c(0,0,0,0), "cm")
-  )
+        axis.ticks.x = element_blank())
 
-pdf("lac_fig.pdf", width = 7, height = 5)
-kinetics <- plot_grid(lac.fig.km,
-          lac.fig.ph,
-          ncol = 1,
-          align = "v",
-          rel_heights = c(1, 1.4)
-          )
-plot_grid(kinetics,
-          lac.fig.temp,
-          lac.fig.pi,
-          ncol = 3,
-          align = "h",
-          labels = c("(A)", "(B)", "(C)"),
-          label_fontfamily = "Helvetica",
-          label_x = 0,
-          hjust = 0,
-          # axis = "b",
-          rel_widths = c(1, 0.5, 0.5)
-)
+#### Arrange panels ####
+
+pdf("lac_fig.pdf", width = onecol, height = onecol * 0.75)
+lac.fig.temp + lac.fig.km + lac.fig.pi + lac.fig.ph +
+  plot_layout(widths = c(1 , 2.5))
 dev.off()
